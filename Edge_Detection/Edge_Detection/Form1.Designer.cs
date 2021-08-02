cc94fb365cd5ab4bb699566ae9590002b922abf0
ShakedShavit <69425857+ShakedShavit@users.noreply.github.com>
2021-08-02 14:13:56 +0300

    Add files via upload

---
+++ Edge_Detection/Edge_Detection/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Edge_Detection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(500, 90);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Upload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 161);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(724, 600);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(742, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(500, 90);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "initiate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(288, 108);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(405, 22);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(212, 21);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Single Border/Double Border";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 48);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(79, 21);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Feature";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(742, 161);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(724, 600);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 81);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(156, 21);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Other Single Border";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(792, 108);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(116, 22);
+            this.numericUpDown2.TabIndex = 7;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(993, 108);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(116, 22);
+            this.numericUpDown3.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(435, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Edge Intensity";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(789, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Clutter Volume";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(990, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Clutter IntensityMin";
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(1126, 108);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(116, 22);
+            this.numericUpDown4.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1123, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Clutter Intensity Max";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1482, 773);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Edge Detection";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Edge_Detection/Edge_Detection/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Edge_Detection
+{
+    public partial class Form1 : Form
+    {
+        Bitmap image1;
+        Bitmap image2;
+        Color[,] copyArray;//copyies the image
+        Color[,] copyArray2;//copyies the copy of the image
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string imageLocation = "";
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "jpg file(*.jpg)|*.jpg| PNG files(*.png)|*.png| All Files(*.*)|*.*";
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    imageLocation = dialog.FileName;
+
+                    pictureBox1.ImageLocation = imageLocation;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An Error Occured", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        void button2_Click(object sender, EventArgs e)
+        {
+            image1 = new Bitmap(pictureBox1.ImageLocation);
+            image2 = new Bitmap(pictureBox1.ImageLocation);
+
+            #region copying the image to an array
+            copyArray = new Color[image1.Size.Width, image1.Size.Height];
+
+            for (int x = 0; x < image1.Width; x++)
+                for (int y = 0; y < image1.Height; y++)
+                    copyArray[x, y] = image1.GetPixel(x, y);
+            #endregion           
+
+            for (int x = 0; x < image1.Width; x++)
+                for (int y = 0; y < image1.Height; y++)
+                {
+                    int r1 = image1.GetPixel(x, y).R;
+                    int g1 = image1.GetPixel(x, y).G;
+                    int b1 = image1.GetPixel(x, y).B;
+                    int r2;
+                    int g2;
+                    int b2;
+
+                    for (int i = 0;  i < 4; i++)
+                    {
+                        if ((i == 0) && (x + 1 < image1.Width))
+                        {
+                            r2 = image1.GetPixel(x + 1, y).R;
+                            g2 = image1.GetPixel(x + 1, y).G;
+                            b2 = image1.GetPixel(x + 1, y).B;
+                            Coloring_Borders(r1, r2, g1, g2, b1, b2, x, y, x + 1, y);
+                        }
+                        if ((i == 1) && (y + 1 < image1.Height))
+                        {
+                            r2 = image1.GetPixel(x, y + 1).R;
+                            g2 = image1.GetPixel(x, y + 1).G;
+                            b2 = image1.GetPixel(x, y + 1).B;
+                            Coloring_Borders(r1, r2, g1, g2, b1, b2, x, y, x, y + 1);
+                        }
+                        else if ((i == 2) && (x + 1 < image1.Width) && (y + 1 < image1.Height))
+                        {
+                            r2 = image1.GetPixel(x + 1, y + 1).R;
+                            g2 = image1.GetPixel(x + 1, y + 1).G;
+                            b2 = image1.GetPixel(x + 1, y + 1).B;
+                            Coloring_Borders(r1, r2, g1, g2, b1, b2, x, y, x + 1, y + 1);
+                        }
+                        else if ((x - 1 >= 0) && (y + 1 < image1.Height))
+                        {
+                            r2 = image1.GetPixel(x - 1, y + 1).R;
+                            g2 = image1.GetPixel(x - 1, y + 1).G;
+                            b2 = image1.GetPixel(x - 1, y + 1).B;
+                            Coloring_Borders(r1, r2, g1, g2, b1, b2, x, y, x - 1, y + 1);
+                        }
+                    }
+                }
+
+            #region copying the array to a second array (array2 = array)
+            copyArray2 = new Color[image1.Size.Width, image1.Size.Height];
+            for (int x = 0; x < image1.Width; x++)
+                for (int y = 0; y < image1.Height; y++)
+                    copyArray2[x, y] = copyArray[x, y];
+            #endregion
+
+            #region Cluteter cleaning
+            if (numericUpDown2.Value > 0)
+            {
+                for (int x = 0; x < image1.Width; x++)
+                    for (int y = 0; y < image1.Height; y++)
+                    {
+                        if (copyArray[x, y].R == 0 && copyArray[x, y].G == 0 && copyArray[x, y].B == 0)
+                        {
+                            int EdgeNoiseCounter = 0;
+                            for (int noise = 1; noise <= numericUpDown2.Value; noise++)
+                            {
+                                if (x + noise < image1.Width)
+                                    if (copyArray[x + noise, y].R == 0 && copyArray[x + noise, y].G == 0 && copyArray[x + noise, y].B == 0)
+                                        EdgeNoiseCounter++;
+
+                                if (y + noise < image1.Height)
+                                    if (copyArray[x, y + noise].R == 0 && copyArray[x, y + noise].G == 0 && copyArray[x, y + noise].B == 0)
+                                        EdgeNoiseCounter++;
+
+                                if (x - noise >= 0)
+                                    if (copyArray[x - noise, y].R == 0 && copyArray[x - noise, y].G == 0 && copyArray[x - noise, y].B == 0)
+                                        EdgeNoiseCounter++;
+
+                                if (y - noise >= 0)
+                                    if (copyArray[x, y - noise].R == 0 && copyArray[x, y - noise].G == 0 && copyArray[x, y - noise].B == 0)
+                                        EdgeNoiseCounter++;
+
+                                if (x + noise < image1.Width && y + noise < image1.Height)
+                                    if (copyArray[x + noise, y + noise].R == 0 && copyArray[x + noise, y + noise].G == 0 && copyArray[x + noise, y + noise].B == 0)
+                                        EdgeNoiseCounter++;
+
+                                if (x + noise < image1.Width && y - noise >= 0)
+                                    if (copyArray[x + noise, y - noise].R == 0 && copyArray[x + noise, y - noise].G == 0 && copyArray[x + noise, y - noise].B == 0)
+                                        EdgeNoiseCounter++;
+
+                                if (x - noise >= 0 && y + noise < image1.Height)
+                                    if (copyArray[x - noise, y + noise].R == 0 && copyArray[x - noise, y + noise].G == 0 && copyArray[x - noise, y + noise].B == 0)
+                                        EdgeNoiseCounter++;
+
+                                if (x - noise >= 0 && y - noise >= 0)
+                                    if (copyArray[x - noise, y - noise].R == 0 && copyArray[x - noise, y - noise].G == 0 && copyArray[x - noise, y - noise].B == 0)
+                                        EdgeNoiseCounter++;
+                            }
+                            //if there are a lot of edge detections or a few edge detection, it erases the edge
+                            if (EdgeNoiseCounter < numericUpDown3.Value || EdgeNoiseCounter > numericUpDown4.Value)
+                                copyArray[x, y] = image1.GetPixel(x, y);
+                        }
+                    }
+            }
+            #endregion
+
+            #region copying the image (image2 = array)
+            for (int x = 0; x < image1.Width; x++)
+                for (int y = 0; y < image1.Height; y++)
+                    image2.SetPixel(x, y, copyArray[x, y]);
+            #endregion
+
+            pictureBox2.Image = image2;
+        }
+
+        void Coloring_Borders(int r1, int r2, int g1, int g2, int b1, int b2, int x1, int y1, int x2, int y2)
+        {
+            Color newColor = Color.FromArgb(0, 0, 0);
+            //int change = (Math.Abs(r2 - r1) + Math.Abs(g2 - g1) + Math.Abs(b2 - b1)) / 3;//the average of the difference between each element of the colors of two following pixels
+            double change = Math.Sqrt(Math.Pow(r2 - r1, 2) + Math.Pow(g2 - g1, 2) + Math.Pow(b2 - b1, 2));
+
+            //regular version - draws border on the brighter side
+            if (!checkBox1.Checked && !checkBox2.Checked && !checkBox3.Checked)
+            {
+                if ((int)change >= numericUpDown1.Value)
+                {
+                    if (r1 + g1 + b1 > r2 + g2 + b2)
+                        copyArray[x1, y1] = newColor;
+                    else
+                        copyArray[x2, y2] = newColor;
+                }
+            }
+            //opposite regular version - draws border on the darker side
+            else if (!checkBox1.Checked && checkBox3.Checked && !checkBox2.Checked)
+            {
+                if ((int)change >= numericUpDown1.Value)
+                {
+                    if (r1 + g1 + b1 < r2 + g2 + b2)
+                        copyArray[x1, y1] = newColor;
+                    else
+                        copyArray[x2, y2] = newColor;
+                }
+            }
+            //another opposite regular version - draws border on both sides
+            else if (checkBox1.Checked && !checkBox2.Checked)
+            {
+                if ((int)change >= numericUpDown1.Value)
+                {
+                    copyArray[x1, y1] = newColor;
+                    copyArray[x2, y2] = newColor;
+                }
+            }
+            //opposie version - it was a mistake but it looks cool so I made it feature - draws a border only if the right side (of the border) is darker
+            else if (checkBox2.Checked)
+            {
+                change = ((r2 - r1) + (g2 - g1) + (b2 - b1)) / 3;
+                if ((int)change >= numericUpDown1.Value)
+                    copyArray[x1, y1] = newColor;
+            }
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            if (numericUpDown2.Value * 6 <= numericUpDown4.Maximum)
+                numericUpDown4.Value = numericUpDown2.Value * 6;
+            if (numericUpDown2.Value / 6 <= numericUpDown4.Maximum)
+                numericUpDown3.Value = numericUpDown2.Value / 6;
+        }
+    }
+}
